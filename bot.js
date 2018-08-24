0c5947d34e7636976bfa10bfcbfba17651f59594
mnsor511 <42652535+mnsor511@users.noreply.github.com>
2018-08-24 19:20:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,3 +29,103 @@ client.user.setGame(`メリオダス 2`,"http://twitch.tv/S-F")
 });
 
 client.login(process.env.BOT_TOKEN);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+const developers = ["303891484481028096"]
+
+const adminprefix = '!';
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+      if (message.content.startsWith(adminprefix + 'ply')) {
+        client.user.setGame(argresult);
+        message.channel.send(`**Ok, playing..** **${argresult}!**`)
+
+    } else
+
+      if (message.content === (adminprefix + "leave")) {
+        message.guild.leave();
+
+    } else
+
+      if (message.content.startsWith(adminprefix + 'wt')) {
+        client.user.setActivity(argresult, {type:'WATCHING'});
+        message.channel.send(`**Ok, watching..** **${argresult}!**`)
+
+    } else
+
+     if (message.content.startsWith(adminprefix + 'ls')) {
+        client.user.setActivity(argresult , {type:'LISTENING'});
+        message.channel.send(`**Ok, listening to..** **${argresult}!**`)
+    } else
+
+     if (message.content.startsWith(adminprefix + 'st')) {
+        client.user.setGame(argresult, "https://www.twitch.tv/idk");
+        message.channel.send(`**Ok, Streaming..** **${argresult}!**`)
+    }
+
+     if (message.content.startsWith(adminprefix + 'set-name')) {
+        client.user.setUsername(argresult).then
+        message.channel.send(`**Changing my name to..** **${argresult}!** `)
+
+    } else
+
+     if (message.content.startsWith(adminprefix + 'set-avatar')) {
+        client.user.setAvatar(argresult);
+        message.channel.send(`**Changing my avatar to..** ${argresult}`);
+
+    } else
+
+    if (message.content.startsWith(adminprefix + 'set-status')) {
+        client.user.setStatus(argresult)
+        message.channel.send(`**Ok, status changed to..** **${argresult}!**`)
+    }
+
+});
