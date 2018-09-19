343f92dc39dbd43adb9f12a8443edd6bc82dc6b7
mnsor511 <42652535+mnsor511@users.noreply.github.com>
2018-09-19 22:13:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -521,7 +521,6 @@ client.on("message", message => { //clear
 
 
 
-client.oN("message", message => {
         if (message.content === "setprefix") {
         if (message.author.id !== "274923685985386496" && !message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`U don't have permission to do that`);
   let args = message.content.split(" ").slice(1);
